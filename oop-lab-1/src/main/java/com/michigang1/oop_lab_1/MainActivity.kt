57fe390f5f1e4032ba7b0a0a83e10ea76ed7862a
Michael Chirozidi <maikl.tigriv@gmail.com>
2022-09-12 19:02:46 +0200

    refactoring

--- oop-lab-1/src/main/java/com/michigang1/oop_lab_1/MainActivity.kt
+++ oop-lab-1/src/main/java/com/michigang1/oop_lab_1/MainActivity.kt
@@ -11,7 +11,6 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        actionBar?.show()
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         binding.apply {
